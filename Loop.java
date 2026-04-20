69fc1c1725b3f7c8bd239967b3715c377629b018
Aman Piyush <amanpiyush.singh@gmail.com>
2026-04-20 06:10:01 +0530

    Today Learn Everything about the Git

---
+++ Loop.java
@@ -0,0 +1,22 @@
+public class Loop {
+    public static void main (String [] args)
+    {
+        for( int i = 1;i <= 10; i++)
+        {
+            System.out.println("Day " + i);
+
+            for(int j = 1; j<=9 ;j++)
+            {
+                System.out.println("Hours " + (j+8) + "-" + (j+9));
+            }
+        }
+
+        
+        // int i = 0;
+        // while (i <= 10)
+        // {
+        //     System.out.println("Hello" + " "+ i);
+        //     i++;
+        // }
+    }
+}
--- Pratical.java
+++ Pratical.java
@@ -1,13 +1,59 @@
 public class Pratical {
     public static void main(String[] args) 
     {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
+        //conditional statement
+        int x = 8;
+        int y = 7;
+        // if (x > 10)
+        // {
+        //     System.out.println("Hello");
+        // }else
+        // {
+        //     System.out.println("Bye");
+        // }
+        if ( x > y)
+            System.out.println("x is greater than y");
+        else
+            System.out.println("x is less than y");
 
-        boolean result = a < b || c > b;
-        System.out.println("Result : " + result);
+
+        //Ternary Operator
+        int a = 10;
+        int result = 0;
+        // result = a%2==0  ? "Even" : "Odd"; //condition ? value if true : value if false
+        System.out.println(result);
+
+
+
+        //Switch case
+        int day = 1;
+        switch (day) 
+        {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
         
     }
 }
