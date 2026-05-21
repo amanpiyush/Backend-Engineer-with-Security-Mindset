06acead36f180fb22dd4e8c4efab22c1711c5477
Aman Piyush <amanpiyush2708@gmail.com>
2026-05-21 21:26:24 +0530

    just

---
+++ Riya.java
@@ -0,0 +1,16 @@
+class Riya{
+
+    public static void main(String[] args) {
+        
+        //primitive datatype
+
+        int num = 79; //values
+        int num2 = 234957231;
+        float num3 = 34.56f;
+        double num4 = 34.56;
+        char Name = 'A';
+        String Name2 = "Riya";
+        System.out.println(num3);
+
+    }
+}
